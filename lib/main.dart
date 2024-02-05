9b2c11ecb53c884bf3db9e78007f886dfdbce68a
ojcarlos <carlos.ojunior00@gmail.com>
2024-02-05 17:35:55 -0400

    ruby added

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("ola mundo!"),
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            titleTextStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.blueGrey[900],
+            title: const Text('I am Rich'),
+          ),
+          body: const Center(
+            child: Image(
+              image: AssetImage('images/rubi.png')
+            ),
+          ),
       ),
     ),
   );
